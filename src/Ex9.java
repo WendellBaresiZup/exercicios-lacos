377487e5d0a7f9a9fa9dc51dc0b35013f7d372ee
Wendell Baresi <wendell.oliveira@zup.com.br>
2024-12-06 11:14:06 -0300

    Finalizando os Exercícios

---
+++ src/Ex9.java
@@ -0,0 +1,9 @@
+public class Ex9 {
+    public static void main(String[] args) {
+        String nBinario = "1010";
+
+        int nDecimal = Integer.parseInt(nBinario,2);
+
+        System.out.println(nDecimal);
+    }
+}
